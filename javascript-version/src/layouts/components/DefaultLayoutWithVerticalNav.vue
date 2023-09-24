1f79f085c9a3abe23812e96bcb439dcdfdd466e8
Jorge Cura <jorge.cura@rt2.com>
2023-09-24 17:52:17 -0400

    "Update yarn.lock file with new registry details"
    The yarn.lock file has been updated to reference new registry URLs for certain dependencies. This change is necessary because the packages have been migrated to a new private npm registry hosted on Azure DevOps Artifacts.

--- javascript-version/src/layouts/components/DefaultLayoutWithVerticalNav.vue
+++ javascript-version/src/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -156,21 +156,7 @@ const upgradeBanner = computed(() => {
     </template>
 
     <template #after-vertical-nav-items>
-      <!-- 👉 illustration -->
-      <a
-        href="https://themeselection.com/item/materio-vuetify-vuejs-admin-template"
-        target="_blank"
-        rel="noopener noreferrer"
-        style="margin-left: 7px;"
-      >
-        <img
-          :src="upgradeBanner"
-          alt="upgrade-banner"
-          transition="scale-transition"
-          class="upgrade-banner mx-auto"
-          style="max-width: 230px;"
-        >
-      </a>
+
     </template>
 
     <!-- 👉 Pages -->
